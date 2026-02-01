e1b4d2343642df74c9d58695317b5a38da524abe
maxy08-ollenv <maxollenviera@gmail.com>
2026-02-01 17:39:11 +0800

    Refactor chat log button event handlers to jQuery

--- web/www/dist/js/app.js
+++ web/www/dist/js/app.js
@@ -47,16 +47,16 @@ function formatAMPM(date) {
     return strTime;
 }
 
-chat_log_button.onclick = () => {
-        chat_log_button.hidden = true;
-        chat_log.hidden = false;
+$("#chat_log_button").click(function () {
+        $("#chat_log_button").hide();
+        $("#chat_log").show();
         window.onresize();
-    };
+    });
 
-chat_log_close.onclick = () => {
-        chat_log_button.hidden = false;
-        chat_log.hidden = true;
-    };
+$("#chat_log_close").click(function () {
+        $("#chat_log_button").show();
+        $("#chat_log").hide();
+    });  
 
 enterFullscreen = (div) => {
     const el = $(div)[0];
