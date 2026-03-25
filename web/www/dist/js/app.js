4591bbb1ea47939517ac482f3184516c968371fb
maxy08-ollenv <maxollenviera@gmail.com>
2026-03-25 17:26:37 +0800

    Update app.js

--- web/www/dist/js/app.js
+++ web/www/dist/js/app.js
@@ -45,6 +45,17 @@ function max (array) {
 
 // https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
 
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    return strTime;
+}
+
 function time() {
     let date = new Date();
     let hours = date.getHours();
